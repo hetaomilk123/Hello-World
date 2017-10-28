700104be3f2d5b27c840a1c7fe3e390fdf4adff4
hetaomilk123 <hetaomilk123@126.com>
2017-10-28 20:24:35 +0800

    Add header file util.h

--- util.h
+++ util.h
@@ -3,3 +3,15 @@ class StrVec{
 public:
   StrVec &operator=(std::initializer_list <std::string>);
 };
+
+class StrBlobPtr{
+public:
+  std::string& operator*() const{
+    auto p = check(curr, "dereference past end");
+    return (*p)[curr];
+  }
+  std::string* operator->() const{
+    return &this->operator*(); 
+  }
+};
+
